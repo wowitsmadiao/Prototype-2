bfcdc5766bc2fd0d5e316a935d0321328c25a169
Madison Adiao <56092617+wowitsmadiao@users.noreply.github.com>
2019-11-25 12:40:40 -0800

    Horizontal Input Detection
    Game detects player input; need to add movement response

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float horizontalInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");   
     }
 }
