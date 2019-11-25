ec808ac7e38ef3b00810a5f3f8a8e36627cabafd
Madison Adiao <56092617+wowitsmadiao@users.noreply.github.com>
2019-11-25 12:47:18 -0800

    Left Boundary Set
    Player is prevented from moving offscreen to the left only.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,6 +16,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.x < -10)
+        {
+            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+        }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);   
     }
