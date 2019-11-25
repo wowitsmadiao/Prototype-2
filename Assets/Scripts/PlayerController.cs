d32b193d0a61a86064976be8e9e7017714ab6b59
Madison Adiao <56092617+wowitsmadiao@users.noreply.github.com>
2019-11-25 13:58:14 -0800

    prefab editing
    went back to video 3 after realizing prefabs were not set up properly. (unfinished)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10.0f;
+    public GameObject projectilePrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -32,6 +33,7 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             //Launch a can from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }  
     }
 }
