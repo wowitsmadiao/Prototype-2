50965b56747d59a67f099da28afd36941738b5ee
Madison Adiao <56092617+wowitsmadiao@users.noreply.github.com>
2019-11-27 10:09:37 -0800

    Food Prefab Added
    Food prefab now works properly.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10.0f;
+    
     public GameObject projectilePrefab;
 
     // Start is called before the first frame update
