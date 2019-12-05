dde33dd36d8e6af6dab7cf5e86ee17edec59bf23
Madison Adiao <56092617+wowitsmadiao@users.noreply.github.com>
2019-12-05 13:56:41 -0800

    Animal Speeds and Range Adjusted
    Animals will now spawn where the player can reach them; Stag speed reduced from 20 to 15.

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-    private float spawnRangeX = 20;
+    private float spawnRangeX = 10;
     private float spawnPosZ = 20;
     private float startDelay = 2;
     private float spawnInterval = 1.5f;
